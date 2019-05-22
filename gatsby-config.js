e0893d8bd33917dce6df0abb7c0c9ccb3aa9ee0a
jonkurtis <jonathankurtis@gmail.com>
2019-05-22 19:41:40 -0400

    Added Google Analytics plugin.

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,6 +5,28 @@ module.exports = {
   },
   plugins: [
     'gatsby-plugin-react-helmet',
+    {
+    resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: "UA-43096313-4",
+        // Defines where to place the tracking script - `true` in the head and `false` in the body
+        head: true,
+        // Setting this parameter is optional
+        anonymize: false,
+        // Setting this parameter is also optional
+        respectDNT: true,
+        // Avoids sending pageview hits from custom paths
+        exclude: ["/preview/**",],
+        // Enables Google Optimize using your container Id
+        optimizeId: "YOUR_GOOGLE_OPTIMIZE_TRACKING_ID",
+        // Enables Google Optimize Experiment ID
+        experimentId: "YOUR_GOOGLE_EXPERIMENT_ID",
+        // Set Variation ID. 0 for original 1,2,3....
+        variationId: "YOUR_GOOGLE_OPTIMIZE_VARIATION_ID",
+        // Any additional create only fields (optional)
+        cookieDomain: "jonkurtis.com",
+      },
+    },
     'gatsby-plugin-sass',
     {
       // keep as first gatsby-source-filesystem plugin for gatsby image support
