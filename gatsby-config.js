aab554893cf6ffe05c5f14e4a38ee93fc86636f0
jonkurtis <jonathankurtis@gmail.com>
2019-05-22 19:48:53 -0400

    Updated site meta.

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby + Netlify CMS Starter',
-    description: 'This repo contains an example business website that is built with Gatsby, and Netlify CMS.It follows the JAMstack architecture by using Git as a single source of truth, and Netlify for continuous deployment, and CDN distribution.',
+    title: 'JonKurtis | Web Developer and Marketer.',
+    description: `Hi I'm Jon Kurtis. Check out my portfolio and blog about web development, digital analytics and SEM/SEO.`,
   },
   plugins: [
     'gatsby-plugin-react-helmet',
