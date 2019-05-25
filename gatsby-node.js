61695d3dfd81a1516c78b165c0a6907d8ec843ec
jonkurtis <jonathankurtis@gmail.com>
2019-05-25 13:56:04 -0400

    Trying to use slug field with or operator for pages that do not have one in the frontmatter.

--- gatsby-node.js
+++ gatsby-node.js
@@ -35,8 +35,7 @@ exports.createPages = ({ actions, graphql }) => {
     posts.forEach(edge => {
       const id = edge.node.id
       createPage({
-        // path: edge.node.fields.slug,
-        path: edge.node.frontmatter.slug,
+        path: edge.node.frontmatter.slug || edge.node.fields.slug,
         tags: edge.node.frontmatter.tags,
         component: path.resolve(
           `src/templates/${String(edge.node.frontmatter.templateKey)}.js`
