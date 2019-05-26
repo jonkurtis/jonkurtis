ae76bd8e71cce907a69876428defcfd8b8978fd9
jonkurtis <jonathankurtis@gmail.com>
2019-05-26 12:20:49 -0400

    Cleaning up contact pages and unused variable in gatsby-node.

--- gatsby-node.js
+++ gatsby-node.js
@@ -79,7 +79,6 @@ exports.onCreateNode = ({ node, actions, getNode }) => {
   if (node.internal.type === `MarkdownRemark`) {
 
     if (node.frontmatter.templateKey === 'blog-post' ) {
-      const value = createFilePath({ node, getNode })
       createNodeField({
         name: `slug`,
         node,
