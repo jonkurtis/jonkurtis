cf0ab785b5a467b08fb61b6aba9bafd1f69186ee
jonkurtis <jonathankurtis@gmail.com>
2023-03-04 23:45:18 -0500

    add daisy ui theme.

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -9,4 +9,15 @@ module.exports = {
 		require('@tailwindcss/typography'),
 
 	],
+	daisyui: {
+		styled: true,
+		themes: true,
+		base: true,
+		utils: true,
+		logs: true,
+		rtl: false,
+		prefix: "",
+		darkTheme: "dark",
+		themes: ["night"]
+	  },
 }
